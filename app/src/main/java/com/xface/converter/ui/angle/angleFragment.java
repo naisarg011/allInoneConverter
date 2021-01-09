9235f120ecca7c4deb8a1145cbea452a71390074
naisarg011 <naisarg011@gmail.com>
2021-01-10 01:02:45 +0530

    bug fixed in angle fragment

--- app/src/main/java/com/xface/converter/ui/angle/angleFragment.java
+++ app/src/main/java/com/xface/converter/ui/angle/angleFragment.java
@@ -4,34 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.xface.converter.R;
-
-import static com.xface.converter.R.id.text_Curr;
+import com.xface.converter.ui.speed.speedFragment;
 
 public class angleFragment extends Fragment {
+
+    private AngleViewModel mViewModel;
+
+    public static speedFragment newInstance() {
+        return new speedFragment();
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        AngleViewModel angleViewModel = new ViewModelProvider(this).get(AngleViewModel.class);
-        View ang = inflater.inflate(R.layout.angle_fragment, container, false);
-        final TextView textView = ang.findViewById(text_Curr);
-       angleViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return ang;
+        return inflater.inflate(R.layout.angle_fragment, container, false);
     }
-
 
 }
